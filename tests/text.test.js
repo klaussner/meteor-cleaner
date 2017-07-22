b64fde877b97ed6fb826805b0e3f0349896d9cd2
Christian Klaussner <developer@klaussner.me>
2017-07-22 21:17:08 +0200

    Improve formatting of large total file size

---
+++ tests/text.test.js
@@ -0,0 +1,12 @@
+const formatBytes = require('../lib/text.js').formatBytes;
+
+describe('Text formatting', () => {
+  test('Small size (MiB)', () => {
+    expect(formatBytes(1024 * 1024)).toBe('1 MiB');
+  });
+
+  test('Large size (GiB)', () => {
+    const onePointFiveGiB = 1024 * 1024 * (1024 + 512);
+    expect(formatBytes(onePointFiveGiB)).toBe('1.50 GiB');
+  });
+});
